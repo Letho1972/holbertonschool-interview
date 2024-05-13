bc7b832e820cf8cb79ff092e2a884e7b0fcd8a45
Letho1972 <ericmarescq@gmail.com>
2024-05-13 10:45:38 +0200

    fix commits

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+"""Write a method that determines if all the boxes can be opened."""
 
 def canUnlockAll(boxes):
-    # Ensemble pour stocker les boîtes déjà ouvertes
+    """ Ensemble pour stocker les boîtes déjà ouvertes"""
     opened_boxes = set()
-    # Ajouter la première boîte (boîte 0) à l'ensemble des boîtes ouvertes
+    """ Ajouter la première boîte (boîte 0) à l'ensemble des boîtes ouvertes"""
     opened_boxes.add(0)
-    # Pile pour suivre les boîtes à explorer
+    """ Pile pour suivre les boîtes à explorer"""
     stack = [0]
-    # Tant qu'il reste des boîtes à explorer dans la pile
+    """ Tant qu'il reste des boîtes à explorer dans la pile """
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
             if key not in opened_boxes:
                 opened_boxes.add(key)
-                # Ajouter la boîte ouverte à la pile pour exploration ultérieure
+                """ Ajouter la boîte ouverte à la pile pour exploration ultérieure"""
                 stack.append(key)
-                # attention à l'indentation du return
+                """ attention à l'indentation du return"""
     return len(opened_boxes) == len(boxes)
             
