974a3f525dcf4b29ea3c3fee9bc88689d155460c
Letho1972 <ericmarescq@gmail.com>
2024-05-31 14:09:09 +0200

    fix 0. Minimum Operations  indentation

--- minimum_operations/0-main.py
+++ minimum_operations/0-main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-Main file for testing
-"""
+Main file for testing"""
 
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
 
--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,17 +6,20 @@ the fewest number of operations needed to result in exactly"""
 
 
 def minOperations(n):
+    """ execute only two operations in this file """
 
     if n <= 1:
+
         return 0
 
     operations = 0
-    factor = 2
+    number = 2
 
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+    while number <= n:
+        if n % number == 0:
+            operations += number
+            n //= number
+        else:
+            number += 1
 
     return operations
