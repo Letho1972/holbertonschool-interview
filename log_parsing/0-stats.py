98979bef015355ca019ad167faa81088407e91c0
Letho1972 <ericmarescq@gmail.com>
2024-06-21 11:14:47 +0200

    fixed documentation

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """
 a script that reads stdin line by line and computes metrics
+sys: provides access to some variables
+defaultdict from collections: that returns a default value
+if the requested key is not found
 """
 
 import sys
 from collections import defaultdict
-
+"""
+Tracks the cumulative size of the files
+Stores the counts of different HTTP status codes
+Counts the number of processed lines
+"""
 total_file_size = 0
 status_codes = defaultdict(int)
 
 line_count = 0
+""" The main loop reads lines from the standard input """
 try:
     for line in sys.stdin:
         line_count += 1
