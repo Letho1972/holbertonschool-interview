b60d05b4a01c5e8de5b5b3561113586ca18da43e
Letho1972 <ericmarescq@gmail.com>
2024-08-19 15:42:01 +0200

    fix betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -34,7 +34,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			curr = curr->next;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev->index, curr->index);
+	printf("Value found between indexes [%lu] and [%lu]\n"
+	, prev->index, curr->index);
 
 	while (prev && prev->index <= curr->index)
 	{
