fd0b12eb8c22e774d5ebcf46815f7c32f4241e65
Letho1972 <ericmarescq@gmail.com>
2024-11-15 09:52:52 +0100

    Island Perimeter

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,66 +1,28 @@
 #!/usr/bin/python3
-""" Island Perimeter mandatory """
+"""
+    Module that calculates the perimeter of an island in a grid
+    The grid is a 2D list where:
+- 0 represents water
+- 1 represents land
 
-
-def test0(n):
-    """
-    *******************************
-    ******test if n is 0 or 1******
-    *******************************
-    @n: number of the grid
-    Returns: 0 if n is 1 or 1 if n is 0
-    """
-    if (n == 0):
-        return 1
-    return 0
+An island is a group of 1's (land) connected either vertically or horizontally.
+The perimeter of the island is defined as the total length of its boundary.
+"""
 
 
 def island_perimeter(grid):
+    """Function that calculates the perimeter of an island in a grid
+       grid (list of list of int): A 2D list representing the grid, where
+       0 represents water and 1 represents land.
     """
-    ***********************************************
-    ******returns the perimeter of the island******
-    ***************described in grid***************
-    ***********************************************
-    @grid: is a list of list of integers:
-                *** 0 represents water
-                *** 1 represents land
-                *** Each cell is square, with a side
-                    length of 1
-                *** Cells are connected horizontally/vertically
-                    (not diagonally).
-                *** grid is rectangular, with its width and
-                    height not exceeding 100
-            The grid is completely surrounded by water
-            There is only one island (or nothing).
-            The island doesn’t have “lakes” (water inside that isn’t
-                connected to the water surrounding the island).
-    """
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
-
-    p = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                                        "grid numbers must be 0 or 1"
+    result = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i-1 < 0:
-                    p += 1
-                else:
-                    p += test0(grid[i-1][j])
-                if j-1 < 0:
-                    p += 1
-                else:
-                    p += test0(grid[i][j-1])
+                result += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    result -= 2
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    result -= 2
 
-                try:
-                    p += test0(grid[i+1][j])
-                except IndexError:
-                    p += 1
-                try:
-                    p += test0(grid[i][j+1])
-                except IndexError:
-                    p += 1
-
-    return p
+    return result
